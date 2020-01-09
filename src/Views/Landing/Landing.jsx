89b10dea4da536202f01f6c12003493bc3085d86
Mark <mrk.rosenberg@gmail.com>
2020-01-09 16:18:29 -0700

    changes background color of app

--- src/Views/Landing/Landing.jsx
+++ src/Views/Landing/Landing.jsx
@@ -7,8 +7,11 @@ import React, { Component } from 'react';
 import LogIn from '../LogIn/LogIn';
 import SignUp from '../SignUp/SignUp';
 
+// StyleSheet
+import './Landing.scss';
+
 // Bootstrap Imports
-import Navbar from 'react-bootstrap/Navbar';
+// import Navbar from 'react-bootstrap/Navbar';
 
 export class Landing extends Component {
 
@@ -51,13 +54,10 @@ export class Landing extends Component {
 
         return (
             <div className="landing-view">
-                <Navbar bg="dark">
-                    <div className="auth-options">
-                        <button onClick={this.logIn}>Log In</button>
-                        <button onClick={this.signUp}>Sign Up</button>
-                    </div>
-                </Navbar>
-                
+                    <header className="auth-options">
+                        <button className="option-button" onClick={this.logIn}>Log In</button>
+                        <button className="option-button" onClick={this.signUp}>Sign Up</button>
+                    </header>
                 { this.state.userStatus === 'login' && <LogIn />}
                 { this.state.userStatus === 'signup' && <SignUp />}
             </div>
